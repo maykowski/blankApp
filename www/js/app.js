984b7b870e3eefff02a7e37fb7d97b5bb7f1db3c
maykowski <wojtek.maykowski@gmail.com>
2017-01-05 22:34:17 +0100

    routing

--- www/js/app.js
+++ www/js/app.js
@@ -22,13 +22,33 @@ angular.module('starter', ['ionic'])
       }
     });
   })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('tabs', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+      .state('tabs.list', {
+          url: '/list',
+          views: {
+            'list-tab': {
+              templateUrl: 'templates/list.html',
+              controller: 'ListController'
+            }
+          }
+        }
+      )
+   $urlRouterProvider.otherwise('/tab/list')
+  })
   .controller('ListController', ['$scope', '$http', function ($scope, $http) {
     $http.get('js/data.json').success(function (data) {
       $scope.artists = data.speakers;
 
 
       $scope.onItemDelete = function (item) {
-        $scope.artists.splice($scope.artists.indexOf(item),1)
+        $scope.artists.splice($scope.artists.indexOf(item), 1)
       };
       $scope.moveItem = function (item, fromIndex, toIndex) {
         // alert(fromIndex + " " + toIndex);
@@ -46,6 +66,6 @@ angular.module('starter', ['ionic'])
 
         })
 
-        }
+      }
     });
   }]);
