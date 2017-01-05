26093c0c6f9c2ba5b29cae615e8878af4e31d876
maykowski <wojtek.maykowski@gmail.com>
2017-01-05 12:51:44 +0100

    reorder search

--- www/js/app.js
+++ www/js/app.js
@@ -5,26 +5,32 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-  .controller('ListController', ['$scope', '$http', function ($scope, $http) {
-    $http.get('js/data.json').success(function(data){
-      $scope.artists = data.speakers;
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
     });
-    $scope.test = 'Test';
+  })
+  .controller('ListController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('js/data.json').success(function (data) {
+      $scope.artists = data.speakers;
+
+
+      $scope.moveItem = function (item, fromIndex, toIndex) {
+        // alert(fromIndex + " " + toIndex);
+        $scope.artists.splice(fromIndex, 1);
+        $scope.artists.splice(toIndex, 0, item);
+      }
+    });
   }]);
